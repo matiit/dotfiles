4c8660e0750959b06bc47cc4c286881e7aca2ee4
Mateusz Tracz <matiqing@gmail.com>
2019-06-30 17:04:02 +0100

    install.sh script will now fetch and install the vundle git plugin.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # dotfiles
 
 Personal "dotfiles".
+
+ - [x] install 'dotfiles' into home dir
+ - [x] install Vundle for vim
+ - [ ] install zsh for current user
--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
+INFOS=();
+
 install () {
     for file in src/*; do
       echo "Installing $file";
       FILENAME=$(basename $file);
-      cp $file ~/.$FILENAME;
+      cp $file /home/$USER/.$FILENAME;
     done;
 }
 
+vundle_install() {
+    if [ -d ~/.vim/bundle/Vundle.vim ]; then
+        echo "Vundle is already insalled. Nothing to do.";
+    else
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+        INFOS+="To finish up vundle installation, open vim and run :VundleInstall command";
+fi
+}
+
 install;
+vundle_install;
+
+for msg in "${INFOS[@]}"
+do
+	echo $msg;
+done
+
 
